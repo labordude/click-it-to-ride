32bf9cabe19e28ecb8cfbfdd768104759dd87937
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-04-27 22:27:35 -0500

    looking for heroku

--- dist/server.js
+++ dist/server.js
@@ -3,11 +3,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
 
-module.exports = server
+module.exports = server;
